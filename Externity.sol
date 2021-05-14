ceb29eb3e9437110da4908111b0e38bc3be4e371
ExternityCEO <84014773+ExternityCEO@users.noreply.github.com>
2021-05-14 19:32:58 +0200

    Update Externity.sol

--- Externity.sol
+++ Externity.sol
@@ -27,7 +27,7 @@ contract SafeMath {
 contract Externity is ERC20Interface, SafeMath {
     string public name;
     string public symbol;
-    uint8 public decimals; // 18 decimals is the strongly suggested default, avoid changing it
+    uint8 public decimals;
 
     uint256 public _totalSupply;
 
